94d49f4cb2119c86c88f639ee90a1a3320d552a6
masonishee <mishee2010@gmail.com>
2023-10-19 18:35:12 -0500

    Fix the create action

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import App from "./App.vue";
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:5000" : "/";
+axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
 createApp(App).mount("#app");
