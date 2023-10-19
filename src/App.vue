32c2e1f3d9cfe11e3687e01091f228eb36bfb4ad
flztjl <ssrsrfy.jl@gmail.com>
2023-10-19 09:48:19 +0800

    Show action

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ export default {
     return {
       posts: [],
       newPostParams: {},
+      currentPost: {},
     };
   },
   created: function () {
@@ -29,6 +30,10 @@ export default {
           console.log("posts create error", error.response);
         });
     },
+    showPost: function (post) {
+      this.currentPost = post;
+      document.querySelector("#post-details").showModal();
+    },
   },
 };
 </script>
@@ -43,7 +48,6 @@ export default {
       <input type="text" v-model="newPostParams.image_url" />
       Comment:
       <input type="text" v-model="newPostParams.comment" />
-
       <button v-on:click="createPost()">Create Post</button>
     </div>
     <h1>All Posts</h1>
@@ -51,7 +55,17 @@ export default {
       <h2>{{ post.user }}</h2>
       <img v-bind:src="post.image_url" v-bind:alt="post.user" />
       <p>Comment {{ post.comment }}</p>
+      <button v-on:click="showPost(post)">More info</button>
     </div>
+    <dialog id="post-details">
+      <form method="dialog">
+        <h1>Post info</h1>
+        <p>User: {{ currentPost.user }}</p>
+        <p>Image: {{ currentPost.image_url }}</p>
+        <p>Comment: {{ currentPost.comment }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
