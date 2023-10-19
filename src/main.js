7b04b8319c190b2d1851c67b04f279c9617c1382
masonishee <mishee2010@gmail.com>
2023-10-18 19:37:28 -0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+createApp(App).mount("#app");
